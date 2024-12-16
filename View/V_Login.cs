a85432d14229e02d475c611e35ff9434ed116912
Rafi Ahmad <232410101007@mail.unej.ac.id>
2024-12-16 18:01:30 +0700

    Coba Login

--- View/V_Login.cs
+++ View/V_Login.cs
@@ -1,42 +1,20 @@
-using PBO_Projek.View;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace PBO_Projek
+namespace PBO_Projek.View
 {
     public partial class V_Login : Form
     {
         public V_Login()
         {
-
             InitializeComponent();
-        }
-        private void Login_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint_1(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void UsernameBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button_login_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
